e77fc309e7f99de5a1678aea596bfe4b48f2f57a
sidharths <sidharthsuresh599@gmail.com>
2024-05-30 10:39:12 +0530

    committed

--- adminapp/models.py
+++ adminapp/models.py
@@ -23,6 +23,7 @@ class Event(models.Model):
     date = models.DateTimeField()
     venue = models.TextField()
     description = models.TextField()
+    image=models.ImageField(null=True)
     
     def __str__(self):
         return self.title
